5b0abf007944c1f141a241373c5a27776e3eeb20
Dhirk07 <81063410+Dhirk07@users.noreply.github.com>
2021-06-20 14:18:47 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import requests
+import random
+import colorama
+from colorama import Fore
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+def namegen():
+    length = random.randint(5, 5) #set your min and max (min, max)
+    letters = "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R",
+    "S","T","U","V","W","X","Y","Z","1","2","3","4","5","6","7","8","9","0","_"
+
+    return ''.join(random.choice(letters) for i in range(length))
+
+duh = ('[Click Me](https://discord.gg/VK9dg6qFWw)')
+SignUp = ('[Here!](https://www.roblox.com/signup)')
+
+choice = input(f'{Fore.RED}[S] Start\n[E] Exit\n\n'f'{Fore.RED}Choice: ')
+
+if choice == 'S':
+    while True:
+        name = namegen()
+        r = requests.get("https://api.roblox.com/users/get-by-username?username=" + name)
+        a = r.text
+        if a.find('Id') == -1:
+            print(f'{Fore.GREEN}' + name + ' is available')
+            open("UserNames.txt", "a").write(name + '\n')
+            #Webhook
+            webhook = DiscordWebhook(url="YOUR-WEBHOOK")#your webhook
+            embed = DiscordEmbed(title='New Username Sniped!', color=0x00e3fd)#stuff
+            embed.add_embed_field(name='Username:', value=f'{name}')#stuff
+            embed.add_embed_field(name='Join for more Cool Stuff', value=f'{duh}')#stuff
+            embed.add_embed_field(name='Register Here!', value=f'{SignUp}')#stuff
+            webhook.add_embed(embed)
+            response = webhook.execute(remove_embeds=True)
